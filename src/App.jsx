e1a3ba0c7c58ba31812bc7bf09658d319194be4c
maksat200 <bakirovmaksat7@gmail.com>
2024-11-19 12:53:47 +0600

    new_class

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,21 @@
+import { useState  , useEffect} from "react";
 import { ProductCard } from "./components";
-const products = [
-  {
-
-    id: 0 , 
-    name: "Product 1" ,
-    category: "Category 1" , 
-    price:200 , 
-    img: "https://s7d1.scene7.com/is/image/dish/iPhone_16_Pro_Desert_Titanium_PDP_Image_Position_1?$ProductBase$&fmt=webp-alpha"
-
-  },
-  {
-    id: 1 , 
-    name: "Product 2" ,
-    category: "Category 1" , 
-    price:300 , 
-    img: "https://s7d1.scene7.com/is/image/dish/iPhone_16_Pro_Desert_Titanium_PDP_Image_Position_1?$ProductBase$&fmt=webp-alpha"
-
-  }
-
-
-]
 function App() {
+  const[Loading , setLoading] = useState(true);
+  const [products , setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>setProducts(json))
+            .finally(() => setLoading(false))
+  } , []) ;
+
+  
+
   return (
     <div>
+      {Loading && <div>Loading...</div>}
       {
         products.map((product) => 
         <ProductCard data  ={product} />
