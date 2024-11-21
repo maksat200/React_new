7e6558e123320025259b1402e24cb4125c5a7512
maksat200 <bakirovmaksat7@gmail.com>
2024-11-21 21:56:43 +0600

    total work

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,10 @@
-import "./index.css" ; 
+import "./index1.css" ; 
 export function ProductCard(props) { 
     const {image , category , title , price} = props.data ; 
     return (
-    <div className="product-card">
+    <div className="product-card">    
         <img width = {200} src = {image} alt="" />
-        <div>{category}</div>
+        <div class = "category">{category}</div>
         <h4>{title}</h4>
         <div class= 'price'><b>${price}</b>  <button>Add Card</button> </div>
         
